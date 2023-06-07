561214d4bbaf91d4793fa6eea1c9942b3fc054fd
Thaina <71820937+thailuar@users.noreply.github.com>
2023-06-07 13:35:27 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Qual a capital da Espanha?",
+      answers: [
+        { option: "Madrid", correct: true },
+        { option: "Barcelona", correct: false },
+        { option: "Lisboa", correct: false },
+        { option: "Sevilha", correct: false }
+      ],
+    },
+    {
+      question: "Qual emissora transmite o BBB?",
+      answers: [
+        { option: "SBT", correct: false },
+        { option: "Globo", correct: true },
+        { option: "Record", correct: false},
+        { option: "Band", correct: false }
+      ],
+    },
+    {
+      question: "Qual dessas não é uma banda de rock?",
+      answers: [
+        { option: "KISS", correct: false },
+        { option: "Slipknot", correct: false },
+        { option: "Avenged Sevenfold", correct: false },
+        { option: "Jonas Brothers", correct: true }
+      ],
+    },
+    {
+      question: "Qual dessas músicas é cantada pela cantora Luisa Sonza?",
+      answers: [
+        { option: "Supera", correct: false },
+        { option: "Culpada", correct: false },
+        { option: "Por causa de você", correct: false },
+        { option: "Anaconda", correct: true }
+      ],
+    },
+    {
+      question: "Qual a capital de Mato Grosso?",
+      answers: [
+        { option: "Brasilia", correct: false },
+        { option: "Campo Grande", correct: false },
+        { option: "Cuiabá", correct: true },
+        { option: "Palmas", correct: false }
+      ],
+    },
+    {
+      question: "Quem é conhecido como o Rei do Futebol?",
+      answers: [
+        { option: "Neymar", correct: false },
+        { option: "Pelé", correct: true },
+        { option: "Messi", correct: false },
+        { option: "Ronaldo", correct: false }
+      ],
+    },
+    {
+      question: "Qual dessas é uma linguagem de marcação?",
+      answers: [
+        { option: "HTML", correct: true },
+        { option: "PHP", correct: false },
+        { option: "CSS", correct: false },
+        { option: "SQL", correct: false }
+      ],
+    },
+    {
+      question: "A musica 'Ciumeira' é cantada por qual dessas cantoras?",
+      answers: [
+        { option: "Ludmilla", correct: false },
+        { option: "Marilia Mendonça", correct: true },
+        { option: "Luisa Sonza", correct: false },
+        { option: "Priscilla Alcantara", correct: false }
+      ],
+    },
+    {
+      question: "Quem é o presidente do Brasil em 2023?",
+      answers: [
+        { option: "Bolsonaro", correct: false },
+        { option: "Haddad", correct: false },
+        { option: "Lula", correct: true },
+        { option: "Collor", correct: false }
+      ],
+    },
+    {
+      question: "Quem é a protagonista da séria 'Hannah Montana'?",
+      answers: [
+        { option: "Miley Cyrus", correct: true },
+        { option: "Selena Gomez", correct: false },
+        { option: "Demi Lovato", correct: false },
+        { option: "Bridgit Mendler", correct: false }
+      ],
+    },
+  ];
